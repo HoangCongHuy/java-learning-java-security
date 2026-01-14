9b7b0d2e8e9b721f7a3246d310187f8001aed973
Hoàng Công Huy <hoangconghuy97@gmail.com>
2026-01-15 00:05:19 +0700

    first commit

---
+++ src/main/java/com/examplecodewithhuy/javasecurity/StudentController.java
@@ -0,0 +1,37 @@
+package com.examplecodewithhuy.javasecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+    List<Student> students = new ArrayList<>(List.of(
+            new Student("Huy", 1),
+            new Student("Ngoc", 2)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student) {
+        students.add(student);
+        return student;
+    }
+
+}
